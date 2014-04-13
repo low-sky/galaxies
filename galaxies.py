371710d3b89f054abdea0f13f252ad210b26b10f
low-sky <rosolowsky@gmail.com>
2014-04-13 16:15:37 -0600

    Added basic query functionality.

--- galaxies.py
+++ galaxies.py
@@ -1,7 +1,8 @@
-import astropy.coordinates as coords
-import astroquery as query
+from astropy.coordinates import ICRS
+from astroquery.ned import Ned
 import astropy.units as u
 from astropy.table import Table
+import warnings
 
 class Galaxy(object):
     def __init__(self, name):
@@ -15,6 +16,21 @@ class Galaxy(object):
         self.distance = None
         self.inclination = None
         self.position_angle = None
+        self.redshift = None
+        self.velocity = None
+        try:
+            t = Ned.query_object(name)
+            if len(t)==1:
+                self.canonical_name = t['Object Name'][0]
+                self.velocity = t['Velocity'][0]*u.km/u.s
+                self.coordinates = ICRS(ra=t['RA(deg)'][0],
+                                        dec=t['DEC(deg)'][0],
+                                        unit=(u.degree,u.degree))
+                self.redshift = t['Redshift'][0]
+        except:
+            warnings.warn("Unsuccessful query to NED")
+            pass
+
         
     def echoes (self, x):
         return x
