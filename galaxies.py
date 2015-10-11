00df61031b134b3b0befa339a53be51bd52fc080
Erik Rosolowsky <rosolowsky@ualberta.ca>
2015-10-10 23:58:30 -0600

    Add disk projection functionality

--- galaxies.py
+++ galaxies.py
@@ -1,12 +1,12 @@
-from astropy.coordinates import ICRS
+from astropy.coordinates import ICRS, SkyCoord, Angle
 from astroquery.ned import Ned
 import astropy.units as u
 from astropy.table import Table
 import warnings
+import numpy as np
 
 class Galaxy(object):
     def __init__(self, name):
-
         self.name = name
 # An astropy coordinates structure
         self.coordinates = None
@@ -17,24 +17,50 @@ class Galaxy(object):
         self.inclination = None
         self.position_angle = None
         self.redshift = None
-        self.velocity = None
+        self.vsys = None
         try:
             t = Ned.query_object(name)
             if len(t)==1:
                 self.canonical_name = t['Object Name'][0]
                 self.velocity = t['Velocity'][0]*u.km/u.s
-                self.coordinates = ICRS(ra=t['RA(deg)'][0],
-                                        dec=t['DEC(deg)'][0],
-                                        unit=(u.degree,u.degree))
+                self.center_position = ICRS(ra=t['RA(deg)'][0],
+                                           dec=t['DEC(deg)'][0],
+                                           unit=(u.degree,u.degree))
                 self.redshift = t['Redshift'][0]
         except:
             warnings.warn("Unsuccessful query to NED")
             pass
+        if name == 'M33':
+            self.name = 'M33'
+            self.distance = 8.4e5*u.pc
+            self.center_position = SkyCoord(23.461667,30.660194,unit=(u.deg,u.deg),frame='fk5')
+            self.position_angle = Angle(202*u.deg)
+            self.inclination = Angle(56*u.deg)
+            self.vsys = -179*u.km/u.s
 
-        
-    def echoes (self, x):
-        return x
-    
+    def radius(self, skycoord = None, ra = None, dec = None, 
+               header = None, returnXY = False):
+        if skycoord:
+            PAs = self.center_position.position_angle(skycoord)
+        elif (ra and dec):
+            Offsets = SkyCoord(ra,dec,unit=(u.deg,u.deg))
+            PAs = self.center_position.position_angle(Offsets)
+        else:
+            warnings.warn('You must specify either RA/DEC or a skycoord')
+        GalPA = PAs - self.position_angle
+        GCDist = Offsets.separation(self.center_position)
+    # Transform into galaxy plane
+        Rplane = self.distance*np.tan(GCDist)
+        Xplane = Rplane * np.cos(GalPA)
+        Yplane = Rplane * np.sin(GalPA)
+        Xgal = Xplane
+        Ygal = Yplane / np.cos(self.inclination)
+        Rgal = (Xgal**2+Ygal**2)**0.5
+        if returnXY:
+            return (Xgal,Ygal)
+        else:
+            return Rgal
+
 # push or pull override table using astropy.table
 
 # Check name equivalencies
